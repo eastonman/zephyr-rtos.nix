ea9149329e2fb88d4e5a6caa2dd59b73eb450106
eastonman <me@eastonman.com>
2025-05-05 10:00:24 +0800

    chore: bump nixpkgs to 24.11
    - bump python to python39
    - run formatter

--- flake.nix
+++ flake.nix
@@ -1,33 +1,39 @@
 {
   description = "Zephyr Project - Development Environment";
 
-  inputs.nixpkgs.url = "nixpkgs/release-23.11";
+  inputs.nixpkgs.url = "nixpkgs/release-24.11";
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlays.default ]; });
-    in
-    {
-      overlays.default = import ./overlay;
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [self.overlays.default];
+      });
+  in {
+    overlays.default = import ./overlay;
 
-      templates = {
-        all-toolchains = {
-          path = ./templates/all-toolchains;
-          description = "Zephyr Project template, using all supported toolchains";
-        };
-
-        arm = {
-          path = ./templates/arm;
-          description = "Zephyr Project template, using arm toolchain";
-        };
+    templates = {
+      all-toolchains = {
+        path = ./templates/all-toolchains;
+        description = "Zephyr Project template, using all supported toolchains";
       };
 
-      packages = forAllSystems (system: import ./pkgs { pkgs = nixpkgsFor.${system}; });
-
-      devShells = forAllSystems (system: {
-        default = nixpkgsFor.${system}.mkZephyrSdk { };
-      });
+      arm = {
+        path = ./templates/arm;
+        description = "Zephyr Project template, using arm toolchain";
+      };
     };
+
+    packages = forAllSystems (system: import ./pkgs {pkgs = nixpkgsFor.${system};});
+
+    devShells = forAllSystems (system: {
+      default = nixpkgsFor.${system}.mkZephyrSdk {};
+    });
+  };
 }
--- pkgs/zephyr-sdk-ng/default.nix
+++ pkgs/zephyr-sdk-ng/default.nix
@@ -1,63 +1,87 @@
-{ stdenv, lib, hostPlatform, fetchurl, autoPatchelfHook, makeBinaryWrapper, xz, python38, which
-, hidapi, libftdi1, libusb1, overrideOpenocd, cmake, wget, file
-, version ? "0.16.5", toolchains ? "all" }:
+{
+  stdenv,
+  lib,
+  hostPlatform,
+  fetchurl,
+  autoPatchelfHook,
+  makeBinaryWrapper,
+  xz,
+  python39,
+  which,
+  hidapi,
+  libftdi1,
+  libusb1,
+  overrideOpenocd,
+  cmake,
+  wget,
+  file,
+  version ? "0.16.5",
+  toolchains ? "all",
+}: let
+  pname = "zephyr-sdk";
+  system = lib.splitString "-" hostPlatform.system;
+  host = "${lib.elemAt system 1}-${lib.elemAt system 0}";
+  base-url = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/";
+  hashes = (import ./zephyr-sdk-hash.nix).${version}.${host};
+  install-toolchains =
+    if toolchains == "all"
+    then
+      builtins.trace "All toolchains will be installed. This may take a long..."
+      (builtins.attrNames hashes.toolchains)
+    else toolchains;
+in
+  stdenv.mkDerivation {
+    inherit pname version;
 
-let pname = "zephyr-sdk";
-    system = lib.splitString "-" hostPlatform.system;
-    host = "${lib.elemAt system 1}-${lib.elemAt system 0}";
-    base-url = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/";
-    hashes = (import ./zephyr-sdk-hash.nix).${version}.${host};
-    install-toolchains = if toolchains == "all"
-      then builtins.trace "All toolchains will be installed. This may take a long..."
-             (builtins.attrNames hashes.toolchains)
-      else toolchains;
+    srcs =
+      [
+        (fetchurl {
+          url = "${base-url}${hashes.minimal.file}";
+          hash = hashes.minimal.hash;
+        })
+      ]
+      ++ (map (toolchain:
+        fetchurl {
+          url = "${base-url}${hashes.toolchains.${toolchain}.file}";
+          hash = hashes.toolchains.${toolchain}.hash;
+        })
+      install-toolchains);
 
-in stdenv.mkDerivation {
-  inherit pname version;
+    sourceRoot = "${pname}-${version}";
 
-  srcs = [
-    (fetchurl {
-      url = "${base-url}${hashes.minimal.file}";
-      hash = hashes.minimal.hash;
-    })
-  ] ++ (map (toolchain: fetchurl {
-    url = "${base-url}${hashes.toolchains.${toolchain}.file}";
-    hash = hashes.toolchains.${toolchain}.hash;
-  }) install-toolchains);
+    nativeBuildInputs = [autoPatchelfHook makeBinaryWrapper which file cmake wget];
+    buildInputs = [xz python39 hidapi libftdi1 libusb1];
 
-  sourceRoot = "${pname}-${version}";
+    postUnpack = ''
+      mv ${lib.concatStringsSep " " install-toolchains} $sourceRoot
+    '';
 
-  nativeBuildInputs = [ autoPatchelfHook makeBinaryWrapper which file cmake wget ];
-  buildInputs = [ xz python38 hidapi libftdi1 libusb1 ];
+    dontAutoPatchelf = true;
+    postFixup = ''
+      autoPatchelf $(find $out/zephyr-sdk -mindepth 1 -maxdepth 1 -type d -not -name sysroots)
 
-  postUnpack = ''
-    mv ${lib.concatStringsSep " " install-toolchains} $sourceRoot
-  '';
+      find $out -type f -perm -a+x -name '*-py' | while read prog; do
+        echo "Wrap python program: $prog"
+        wrapProgram $prog \
+          --set NIX_PYTHONPREFIX ${python39} \
+          --set NIX_PYTHONEXECUTABLE ${python39}/bin/python3.9 \
+          --set NIX_PYTHONPATH ${python39}/lib/python3.9/site-packages
+      done
+    '';
 
-  dontAutoPatchelf = true;
-  postFixup = ''
-    autoPatchelf $(find $out/zephyr-sdk -mindepth 1 -maxdepth 1 -type d -not -name sysroots)
-
-    find $out -type f -perm -a+x -name '*-py' | while read prog; do
-      echo "Wrap python program: $prog"
-      wrapProgram $prog \
-        --set NIX_PYTHONPREFIX ${python38} \
-        --set NIX_PYTHONEXECUTABLE ${python38}/bin/python3.8 \
-        --set NIX_PYTHONPATH ${python38}/lib/python3.8/site-packages
-    done
-  '';
-
-  configurePhase = "true";
-  buildPhase = "true";
-  installPhase = ''
-    install -d -m0755 $out/zephyr-sdk
-    mv * $out/zephyr-sdk
-    patchShebangs --host $out/zephyr-sdk
-    (cd $out/zephyr-sdk && ./setup.sh -h ${lib.concatMapStringsSep " " (toolchain: "-t ${toolchain}") install-toolchains})
-    #find -maxdepth 1 -! -name '*.sh' -exec cp -r {} $out/zephyr-sdk \;
-  '' + (lib.optionalString (overrideOpenocd != null) ''
-    rm -f $out/zephyr-sdk/sysroots/x86_64-pokysdk-linux/usr/bin/openocd
-    ln -s ${overrideOpenocd}/bin/openocd \
-       $out/zephyr-sdk/sysroots/x86_64-pokysdk-linux/usr/bin/openocd
-  '');
-}
+    configurePhase = "true";
+    buildPhase = "true";
+    installPhase =
+      ''
+        install -d -m0755 $out/zephyr-sdk
+        mv * $out/zephyr-sdk
+        patchShebangs --host $out/zephyr-sdk
+        (cd $out/zephyr-sdk && ./setup.sh -h ${lib.concatMapStringsSep " " (toolchain: "-t ${toolchain}") install-toolchains})
+        #find -maxdepth 1 -! -name '*.sh' -exec cp -r {} $out/zephyr-sdk \;
+      ''
+      + (lib.optionalString (overrideOpenocd != null) ''
+        rm -f $out/zephyr-sdk/sysroots/x86_64-pokysdk-linux/usr/bin/openocd
+        ln -s ${overrideOpenocd}/bin/openocd \
+           $out/zephyr-sdk/sysroots/x86_64-pokysdk-linux/usr/bin/openocd
+      '');
+  }
